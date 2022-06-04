45009c1aa835a6a5ce705de0b544c8899e7804fc
Angel Sainero <angelsainero@gmail.com>
2022-06-04 12:14:09 +0200

    ejemplosfunciones

---
+++ ejemplosfunciones.py
@@ -0,0 +1,10 @@
+def nombredelafuncion(parametro):
+    resutado = ""
+    for variable in iterable:
+        if variable in "xxxx"
+
+
+def funcion():
+    return "hola mundo"
+frase=funcion()
+print(frase)
