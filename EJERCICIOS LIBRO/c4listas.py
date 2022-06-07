51225b7cf2203558ce720eb074fd0cd4780e4daf
Angel Sainero <angelsainero@gmail.com>
2022-06-07 18:40:05 +0200

    ejercicio tema 4

---
+++ EJERCICIOS LIBRO/c4listas.py
@@ -0,0 +1,25 @@
+'''
+IMPRIMIR LISTA E UNO POR UNO CON LOS NUMEROS IMPARES DEL 1 AL 20 
+'''
+listaimpares=list(range(1,20,2))
+print(listaimpares)
+for impar in listaimpares:
+    print(impar)
+
+        # 1
+        # 4
+        # 7
+        # 10
+        # 13
+        # 16
+        # 19
+
+
+'''
+IMPIMIR LISTA DE LOS MULTIPLOS DE TRES HASTA DESDE EL 3 AL 30 
+'''
+
+threes = list(range(3, 31, 3))
+
+for number in threes:
+    print(number)
