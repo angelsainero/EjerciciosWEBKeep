2a3c53a090c526314159aa40267cf2f9219d6297
Angel Sainero <angelsainero@gmail.com>
2022-06-07 16:52:05 +0200

    Uso de range para crear listas

--- bucles.py
+++ bucles.py
@@ -32,10 +32,7 @@ def extrae1(nombre):
     print (textosalida)
 
 #-----------------------------------------------------------------------------------------------------
-#multiplica
-def multi(numero):
-    for n in range (1,11):
-        print ("{:>2} x {} = {:>2}".format(n, numero, n*numero))
+
         
 #---------------------------------------------------------------------------------------------------
 #Diccionarios iteracion
@@ -81,4 +78,59 @@ def nota_numerica (letra):
     
     return notas[puntero]
 
+#---------------------------------------------------------------------------------------------------------
+magicians = ['alice', 'david', 'carolina'] 
+for magician in magicians: 
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+    
+print("Thank you, everyone. That was a great magic show!")
+
+    # Alice, that was a great trick!
+    # I can't wait to see your next trick, Alice.
+
+    # David, that was a great trick!
+    # I can't wait to see your next trick, David.
+
+    # Carolina, that was a great trick!
+    # I can't wait to see your next trick, Carolina.
+     # Thank you, everyone. That was a great magic show!
+
+#-----------------------------------------------------------------------------------------------------------
+
+   
+
+#Usar range()
+for value in range (1,6):
+    print (value)
+    # 1
+    # 2
+    # 3
+    # 4
+    # 5
+
+def multi(numero):
+    for n in range (1,11):
+        print ("{:>2} x {} = {:>2}".format(n, numero, n*numero))
+    #  1 x 5 =  5
+    #  2 x 5 = 10
+    #  3 x 5 = 15
+    #  4 x 5 = 20
+    #  5 x 5 = 25
+    #  6 x 5 = 30
+    #  7 x 5 = 35
+    #  8 x 5 = 40
+    #  9 x 5 = 45
+    # 10 x 5 = 50
+
+# Uso de range () para crear lista de numeros
+    # Queremos crear una lista que a partir de un range de 1 a 11 cree una lista con su cuadrado
+
+lista=[]
+for valor in range(1,11):
+    cuadrado=valor**2
+    lista.append(cuadrado)
+
+print(lista)
+    # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]        
 
